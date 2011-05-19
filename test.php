3a16a52b2fc66bd55ed54f8f6cdb9b06281b1c8a
Marat Nepomnyashy <maratbn@gmail.com>
2011-05-18 17:28:57 -0700

    Added letters entered printout.

--- test.php
+++ test.php
@@ -5,7 +5,12 @@
     <title>CAPTCHA test</title>
   </head>
   <body>
-    <img src='captcha.php'>
+    <p>
+      Letters entered: <?=$_POST['validate']?>
+    </p>
+    <p>
+      <img src='captcha.php'>
+    </p>
     <form method='POST'>
       Enter letters above:<br>
       <input type='text' name='validate'>
