b3cdef853f034e3313ffd0159685a2e246a73472
Marat Nepomnyashy <maratbn@gmail.com>
2011-05-18 17:34:25 -0700

    Determining if the CAPTCHA is valid or not.

--- test.php
+++ test.php
@@ -9,6 +9,19 @@
       Letters entered: <?=$_POST['validate']?>
     </p>
     <p>
+      <?php
+          //  Based on example at:
+          //  http://www.ejeliot.com/pages/php-captcha
+
+          require('php-captcha.inc.php');
+          if (PhpCaptcha::Validate($_POST['validate'])) {
+              echo 'Valid code entered';
+          } else {
+              echo 'Invalid code entered';
+          }
+      ?>
+    </p>
+    <p>
       <img src='captcha.php'>
     </p>
     <form method='POST'>
