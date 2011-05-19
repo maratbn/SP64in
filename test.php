e1cf44c168901aefe6a17b24bd66fe69a1e4e927
Marat Nepomnyashy <maratbn@gmail.com>
2011-05-18 23:13:27 -0700

    Programmatically creating CAPTCH entry panel via JavaScript.

--- test.php
+++ test.php
@@ -6,67 +6,78 @@
   </head>
   <body>
     <a id='aSendEmail' href='#'>Send Email</a>
-    <div id='divCEntry'>
-      <div id='divCAPTCHA' style='position:relative;width:250px;height:70px'>
-      </div>
-      <p>
-        Enter letters above:<br>
-        <input type='text' id='inputValidate'>
-        <button id='buttonSubmit'>Submit</button>
-      </p>
-    </div>
   </body>
   <script type='text/javascript' src='/toolkits/jquery/jquery-1.5.min.js'>
   </script>
   <script type='text/javascript'>
       $(document).ready(function($) {
-          var totalInvalid = 0;
-          function refreshCAPTCHA() {
-              $('#divCAPTCHA').css(
-                  'background-image',
-                  "url('captcha.php?" + (new Date()).getTime() + "_" +
-                                                        totalInvalid + "')");
-          }
+          function insertCAPTCHA() {
+              var divCEntry = $("<div />");
+              var divCAPTCHA = $("<div style='"
+                          + "position:relative;width:250px;height:70px' />");
+              var inputValidate = $("<input type='text'>");
+              var buttonSubmit = $("<button>Submit</button>");
+              
+              divCEntry
+                  .append(divCAPTCHA)
+                  .append(
+                      $("<p>Enter letters above:<br></p>")
+                          .append(inputValidate)
+                          .append(buttonSubmit));
 
-          function refreshInput() {
-              refreshCAPTCHA();
-              $('#inputValidate').val("");
-          }
+              $('body').append(divCEntry);
 
-          function updateEmailAddress(data) {
-              var strEmail = data && data.email;
-              $('#aSendEmail')
-                  .attr(
-                      'href',
-                      strEmail ? 'mailto://' + strEmail : "#")
-                  .text(
-                      strEmail || "Unable to determine email.");
-          }
 
-          refreshCAPTCHA();
+              var totalInvalid = 0;
+              function refreshCAPTCHA() {
+                  divCAPTCHA.css(
+                      'background-image',
+                      "url('captcha.php?" + (new Date()).getTime() + "_" +
+                                                         totalInvalid + "')");
+              }
 
-          $('#buttonSubmit').click(function() {
-                  $.ajax({
-                          url: 'captcha_validator.php',
-                          type: 'POST',
-                          data: {'validate': $('#inputValidate').val()},
-                          dataType: 'json',
-                          error: function() {
-                                  alert("Encountered error making XHR request"
-                                          + " to server.");
-                              },
-                          success: function(data) {
-                                  var isValid = data && data.is_valid;
-                                  if (isValid) {
-                                      $('#divCEntry').css('display', 'none');
-                                      updateEmailAddress(data);
-                                  } else {
-                                      totalInvalid++;
-                                      refreshInput();
+              function refreshInput() {
+                  refreshCAPTCHA();
+                  inputValidate.val("");
+              }
+
+              function updateEmailAddress(data) {
+                  var strEmail = data && data.email;
+                  $('#aSendEmail')
+                      .attr(
+                          'href',
+                          strEmail ? 'mailto://' + strEmail : "#")
+                      .text(
+                          strEmail || "Unable to determine email.");
+              }
+
+              refreshInput();
+
+              buttonSubmit.click(function() {
+                      $.ajax({
+                              url: 'captcha_validator.php',
+                              type: 'POST',
+                              data: {'validate': inputValidate.val()},
+                              dataType: 'json',
+                              error: function() {
+                                      alert("Encountered error making XHR"
+                                                     + " request to server.");
+                                  },
+                              success: function(data) {
+                                      var isValid = data && data.is_valid;
+                                      if (isValid) {
+                                          divCEntry.css('display', 'none');
+                                          updateEmailAddress(data);
+                                      } else {
+                                          totalInvalid++;
+                                          refreshInput();
+                                      }
                                   }
-                              }
-                      })
-              });
+                          })
+                  });
+          }
+
+          insertCAPTCHA();
       });
   </script>
 </html>
