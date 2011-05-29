e8774f641c42657f77082df8c8ed5cab3f6417cb
Marat Nepomnyashy <maratbn@gmail.com>
2011-05-28 18:47:53 -0700

    Better qTip positioning.

--- @ntisp@m.js
+++ @ntisp@m.js
@@ -137,6 +137,8 @@ $(document).ready(function($) {
                 content: $("<div />").append(divClickToShow)
                                                     .append(divCAPTCHA),
                 position: {
+                    at: 'bottom middle',
+                    my: 'top middle',
                     viewport: $(window)
                 },
                 show: {
