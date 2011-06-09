c1abcf94eeb64997e665ac090a2593a99aafdbcc
Marat Nepomnyashy <maratbn@gmail.com>
2011-06-09 01:48:16 -0700

    Caching retrieved email address in session cookie.

--- @ntisp@m.js
+++ @ntisp@m.js
@@ -4,6 +4,13 @@
  *
  *  Copyright (c) 2011 Marat Nepomnyashy    maratbn@gmail
  *
+ *  Includes the following embedded 3-rd party code:
+ *
+ *      jquery.cookie.js
+ *      https://github.com/carhartl/jquery-cookie
+ *      Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+ *      Used under MIT license
+ *
  *  Module:         @ntisp@m.js
  *
  *  Description:    JavaScript logic that activates anchor tags / links with
@@ -39,6 +46,114 @@
 $(document).ready(function($) {
 
     /**
+     *  The following is Klaus Hartl's jquery.cookie.js library pasted in with
+     *  minor modifications.
+     *
+     *  https://github.com/carhartl/jquery-cookie
+     *  Used under MIT license.
+     *  https://github.com/carhartl/jquery-cookie/raw/master/jquery.cookie.js
+     *  head: 001687708c9270d399eea2d0c4c10fd6b0169b3e
+     */
+    //  ======================================================================
+        /**
+         * jQuery Cookie plugin
+         *
+         * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+         * Dual licensed under the MIT and GPL licenses:
+         * http://www.opensource.org/licenses/mit-license.php
+         * http://www.gnu.org/licenses/gpl.html
+         *
+         */
+
+        // TODO JsDoc
+
+        /**
+         * Create a cookie with the given key and value and other optional parameters.
+         *
+         * @example $.cookie('the_cookie', 'the_value');
+         * @desc Set the value of a cookie.
+         * @example $.cookie('the_cookie', 'the_value', { expires: 7, path: '/', domain: 'jquery.com', secure: true });
+         * @desc Create a cookie with all available options.
+         * @example $.cookie('the_cookie', 'the_value');
+         * @desc Create a session cookie.
+         * @example $.cookie('the_cookie', null);
+         * @desc Delete a cookie by passing null as value. Keep in mind that you have to use the same path and domain
+         *       used when the cookie was set.
+         *
+         * @param String key The key of the cookie.
+         * @param String value The value of the cookie.
+         * @param Object options An object literal containing key/value pairs to provide optional cookie attributes.
+         * @option Number|Date expires Either an integer specifying the expiration date from now on in days or a Date object.
+         *                             If a negative value is specified (e.g. a date in the past), the cookie will be deleted.
+         *                             If set to null or omitted, the cookie will be a session cookie and will not be retained
+         *                             when the the browser exits.
+         * @option String path The value of the path atribute of the cookie (default: path of page that created the cookie).
+         * @option String domain The value of the domain attribute of the cookie (default: domain of page that created the cookie).
+         * @option Boolean secure If true, the secure attribute of the cookie will be set and the cookie transmission will
+         *                        require a secure protocol (like HTTPS).
+         * @type undefined
+         *
+         * @name $.cookie
+         * @cat Plugins/Cookie
+         * @author Klaus Hartl/klaus.hartl@stilbuero.de
+         */
+
+        /**
+         * Get the value of a cookie with the given key.
+         *
+         * @example $.cookie('the_cookie');
+         * @desc Get the value of a cookie.
+         *
+         * @param String key The key of the cookie.
+         * @return The value of the cookie.
+         * @type String
+         *
+         * @name $.cookie
+         * @cat Plugins/Cookie
+         * @author Klaus Hartl/klaus.hartl@stilbuero.de
+         */
+        //  2011-05-30  maratbn
+        //  Don't want to mess with the jQuery namespace in case the web page
+        //  this is running on wants to include the jquery.cookie.js library
+        //  (or some other library that registers itself as $.cookie)
+        //  explicitly.
+        //  jQuery.cookie = function (key, value, options) {
+        khartl_cookie = function (key, value, options) {
+            
+            // key and at least value given, set cookie...
+            if (arguments.length > 1 && String(value) !== "[object Object]") {
+                options = jQuery.extend({}, options);
+
+                if (value === null || value === undefined) {
+                    options.expires = -1;
+                }
+
+                if (typeof options.expires === 'number') {
+                    var days = options.expires, t = options.expires = new Date();
+                    t.setDate(t.getDate() + days);
+                }
+                
+                value = String(value);
+                
+                return (document.cookie = [
+                    encodeURIComponent(key), '=',
+                    options.raw ? value : encodeURIComponent(value),
+                    options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                    options.path ? '; path=' + options.path : '',
+                    options.domain ? '; domain=' + options.domain : '',
+                    options.secure ? '; secure' : ''
+                ].join(''));
+            }
+
+            // key and possibly options given, get cookie...
+            options = value || {};
+            var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+            return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
+        };
+    //  ======================================================================
+
+
+    /**
      *  Creates a new qTip and returns its API object.
      */
     function createQT(elTarget, content) {
@@ -68,6 +183,21 @@ $(document).ready(function($) {
             }).qtip('api');
     }
 
+    /**
+     *  Returns cached email.
+     */
+    function getCachedEmail() {
+        return khartl_cookie('@ntosp@m_email');
+    }
+
+    /**
+     *  Returns value of session cookie '@ntosp@m_cvalid', which should be
+     *  "yes" if CAPTCHA has been solved.
+     */
+    function getCachedCSolved() {
+        return khartl_cookie('@ntosp@m_csolved');
+    }
+    
     function attachCAPTCHA(aSendEmail) {
 
         var elEvents = $("<span />");
@@ -130,8 +260,10 @@ $(document).ready(function($) {
                 inputValidate.focus();
             }
 
-            function updateEmailAddress(data) {
-                var strEmail = data && data.email;
+            function updateEmailAddress() {
+                if (getCachedCSolved() != 'yes') return;
+
+                var strEmail = getCachedEmail();
                 aSendEmail
                     .attr(
                         'href',
@@ -139,7 +271,7 @@ $(document).ready(function($) {
                     .text(
                         strEmail || "Unable to determine email.");
 
-                $(elEvents).trigger('got_@ntisp@m_email', data);
+                $(elEvents).trigger('got_@ntisp@m_email');
             }
 
             /**
@@ -169,8 +301,13 @@ $(document).ready(function($) {
                         success: function(data) {
                                 var isValid = data && data.is_valid;
                                 if (isValid) {
+                                    khartl_cookie(
+                                        '@ntosp@m_email',
+                                        data && data.email || "");
+                                    khartl_cookie('@ntosp@m_csolved', 'yes');
+
                                     divStatus.text("Validated successfully.");
-                                    updateEmailAddress(data);
+                                    updateEmailAddress();
                                 } else {
                                     divStatus.text("Incorrect letters " +
                                                "entered.  Please try again.");
@@ -189,6 +326,7 @@ $(document).ready(function($) {
                 });
 
             refreshInput();
+            updateEmailAddress();
 
             return {
                     parent: divCEntry,
@@ -205,24 +343,23 @@ $(document).ready(function($) {
         var qapiCAPTCHA = createQT(aSendEmail, captcha.parent);
 
         var isCShown = false;
-        var strEmail = "";
 
         aSendEmail.bind('mouseover', function() {
-                if (strEmail) return;
+                if (getCachedEmail()) return;
 
                 if (!isCShown) {
                     qapiClickToReveal.show();
                 }
             });
         aSendEmail.bind('mouseout', function() {
-                if (strEmail) return;
+                if (getCachedEmail()) return;
 
                 if (!isCShown) {
                     qapiClickToReveal.hide();
                 }
             });
         aSendEmail.bind('click', function() {
-                if (strEmail) return;
+                if (getCachedEmail()) return;
 
                 if (isCShown) {
                     qapiCAPTCHA.hide();
@@ -238,8 +375,7 @@ $(document).ready(function($) {
                 return false;
             });
 
-        $(elEvents).bind('got_@ntisp@m_email', function(e, data) {
-                strEmail = data && data.email;
+        $(elEvents).bind('got_@ntisp@m_email', function(e) {
                 qapiCAPTCHA.hide();
             });
     }
