94e0ee3c3177471745712b05d6293822e0744455
Marat Nepomnyashy <maratbn@gmail.com>
2011-06-10 01:02:49 -0700

    Using <ul> to list emails.

--- test.php
+++ test.php
@@ -7,11 +7,14 @@
         href='/components/@ntisp@m/./toolkits/jquery/jquery.qtip-nightly.custom/nightly-0b294b/jquery.qtip.css'>
   </head>
   <body>
-    <span data-antispam='true'></span>
-
-    <br>
-
-    <a href='#' data-antispam='true'>Send Email to Somebody</a>
+    <ul>
+      <li>
+        <span data-antispam='true'></span>
+      </li>
+      <li>
+        <a href='#' data-antispam='true'>Send Email to Somebody</a>
+      </li>
+    </ul>
   </body>
   <script type='text/javascript' src='/toolkits/jquery/jquery-1.5.min.js'>
   </script>
