9534cbb5be3b5af581a063e05fc594645b1593a6
Marat Nepomnyashy <maratbn@gmail.com>
2011-06-12 21:17:29 -0700

    Added example keyed email addresses.

--- email.conf.php
+++ email.conf.php
@@ -1,3 +1,17 @@
 <?php
     $email_default = "maratbn@example.com";
+
+    $emails_keyed = array(
+            'webmaster'     => 'joe_the_webmaster@example.com',
+            'admin'         => 'tom_the_admin@example.com',
+            'president'     => 'roy@example.com',
+            'vpresident'    => 'franklin@example.com',
+            'ceo'           => 'art@example.com',
+            'treasurer'     => 'max@example.com',
+            'pubrelations'  => 'roz@example.com',
+            'sales'         => 'rudy@example.com',
+            'product'       => 'violet@example.com',
+            'marketing'     => 'clark@example.com',
+            'engineering'   => 'brad@example.com'
+        );
 ?>
