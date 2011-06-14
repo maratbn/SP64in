ed5696c56f91fbc42e92219bdbc2db9f58b32f50
Marat Nepomnyashy <maratbn@gmail.com>
2011-06-13 21:06:03 -0700

    Refactored the way the 'validate' POST arg is processed.

--- validator.php
+++ validator.php
@@ -1,9 +1,9 @@
 <?php
-    function isCaptchaValid() {
+    function isCaptchaValid($arg_validate) {
         //  Based on example at:
         //  http://www.ejeliot.com/pages/php-captcha
         require('php-captcha.inc.php');
-        return PhpCaptcha::Validate($_POST['validate']) ? True : False;
+        return PhpCaptcha::Validate($arg_validate) ? True : False;
     }
 
     # Sets email and recall data on the output hash map.
@@ -22,11 +22,18 @@
 
     header('Content-Type: application/json');
 
-    $isValid = isCaptchaValid();
+    $output = array();
 
-    $output = array('is_valid' => $isValid);
+    $flagOKtoOutputData = False;
 
-    if ($isValid) {
+    $arg_validate = $_POST['validate'];
+    if ($arg_validate) {
+        $isValid = isCaptchaValid($arg_validate);
+        $output['is_valid'] = $isValid;
+        if ($isValid) $flagOKtoOutputData = True;
+    }
+
+    if ($flagOKtoOutputData) {
         setEmailData($output);
     }
 
