735c4b1fb6bc2f97ba1813d1b04305ac51c4d9b2
Marat Nepomnyashy <maratbn@gmail.com>
2011-06-29 23:23:09 -0700

    Changed title 'usage example' => 'Usage Examples'.

--- example.php
+++ example.php
@@ -5,7 +5,7 @@
 
 <html>
   <head>
-    <title>SP@in usage example</title>
+    <title>SP@in Usage Examples</title>
     <link rel='stylesheet'
         href='/components/sp@in/./toolkits/jquery/jquery.qtip-nightly.custom/nightly-0b294b/jquery.qtip.css'>
     <style>
