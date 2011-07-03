64a20e25af8620b111548bf9ff96111e473450dd
Marat Nepomnyashy <maratbn@gmail.com>
2011-07-02 20:11:49 -0700

    Realized that the Purisa-Oblique font is GPL, so had to switch to OFL Pecita.

--- php-captcha/captcha_img.php
+++ php-captcha/captcha_img.php
@@ -3,7 +3,7 @@
     //  http://www.ejeliot.com/pages/php-captcha
 
     require('php-captcha.inc.php');
-    $aFonts = array('./Purisa-Oblique.ttf');
+    $aFonts = array('./Pecita.otf');
     $oVisualCaptcha = new PhpCaptcha($aFonts, 250, 70);
     $oVisualCaptcha->Create();
 ?>
