ca38a2c52c00ceafe7c2d2642ea2151d621666b8
Marat Nepomnyashy <maratbn@gmail.com>
2012-12-19 21:59:58 -0800

    Upgrading from qTip2 nightly 0b294b to 365741.

--- example.php
+++ example.php
@@ -7,7 +7,7 @@
   <head>
     <title>SP@in (SP64in) Usage Examples</title>
     <link rel='stylesheet'
-        href='/components/sp@in/toolkits/jquery/jquery.qtip-nightly.custom/nightly-0b294b/jquery.qtip.css'>
+        href='/components/sp@in/toolkits/jquery/jquery.qtip-nightly.custom/nightly-365741/jquery.qtip.css'>
     <style>
       .as_bold {
           font-weight:      bold;
@@ -167,7 +167,7 @@
         src='/components/sp@in/toolkits/jquery/jquery-1.8.3.js'>
     </script>
     <script type='text/javascript'
-        src='/components/sp@in/toolkits/jquery/jquery.qtip-nightly.custom/nightly-0b294b/jquery.qtip.js'>
+        src='/components/sp@in/toolkits/jquery/jquery.qtip-nightly.custom/nightly-365741/jquery.qtip.js'>
     </script>
     <script type='text/javascript' src='/components/sp@in/sp@in.js'>
     </script>
