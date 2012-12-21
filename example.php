ca6fd6015a75b819b7ba98e7886c1e159e6f5986
Marat Nepomnyashy <maratbn@gmail.com>
2012-12-20 20:24:52 -0800

    Using 'sp@in-v0.1.5-min.js' in 'example.php'.

--- example.php
+++ example.php
@@ -173,7 +173,7 @@
         src='/components/sp@in/toolkits/jquery/jquery.qtip-nightly.custom/nightly-365741/jquery.qtip.min.js'>
     </script>
     <script type='text/javascript'
-        src='/components/sp@in/sp@in.js'>
+        src='/components/sp@in/sp@in-v0.1.5-min.js'>
     </script>
   </body>
 </html>
