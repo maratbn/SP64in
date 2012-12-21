eb00ccf9dafe92230f51c2aa865a05b8eb66e340
Marat Nepomnyashy <maratbn@gmail.com>
2012-12-20 16:54:25 -0800

    Informing the user that MS IE version < 7 is not supported on mouseover, and not popping up the CAPTCHA challenge on click.

--- sp@in.js
+++ sp@in.js
@@ -441,9 +441,16 @@ $(document).ready(function($) {
 
         updateEmailAddress();
 
+        var isIEUnder7 = $.browser.msie && $.browser.version < 7;
+
         var qapiClickToReveal = createQT(
                             aSendEmail,
-                            $("<div>Click to reveal email address...</div>"),
+                            $(["<div>",
+                                    isIEUnder7
+                                        ? "MS IE version < 7 not supported.  \
+Please use a more modern web browser."
+                                        : "Click to reveal email address...",
+                                "</div>"].join("")),
                             {
                                 show: { effect: true },
                                 hide: { effect: true }
@@ -509,6 +516,8 @@ $(document).ready(function($) {
                 }
             });
         aSendEmail.bind('click', function() {
+                if (isIEUnder7) return false;
+
                 if (getEmail()) return;
 
                 if (isCShown) {
